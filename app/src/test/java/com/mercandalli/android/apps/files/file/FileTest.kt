f4638fdb452c6741a6050f2b3632897e75c353c4
Mercandj <modjow.jm9@gmail.com>
2018-05-08 00:32:35 +0200

    [FileRow] :recycle: Test the row presenter: copy/cut. (:tada: first tests)

---
+++ app/src/test/java/com/mercandalli/android/apps/files/file/FileTest.kt
@@ -0,0 +1,20 @@
+package com.mercandalli.android.apps.files.file
+
+import com.mercandalli.sdk.files.api.File
+
+class FileTest {
+
+    companion object {
+
+        @JvmStatic
+        fun createFakeFile(): File {
+            return File(
+                    "id",
+                    "path",
+                    "parentPath",
+                    false,
+                    "name"
+            )
+        }
+    }
+}
---
+++ app/src/test/java/com/mercandalli/android/apps/files/file_row/FileRowPresenterTest.kt
@@ -0,0 +1,61 @@
+package com.mercandalli.android.apps.files.file_row
+
+import com.mercandalli.android.apps.files.file.FileTest
+import com.mercandalli.sdk.files.api.FileCopyCutManager
+import com.mercandalli.sdk.files.api.FileDeleteManager
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+class FileRowPresenterTest {
+
+    @Mock
+    private var screen: FileRowContract.Screen? = null
+    @Mock
+    private var fileDeleteManager: FileDeleteManager? = null
+    @Mock
+    private var fileCopyCutManager: FileCopyCutManager? = null
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun onCopyClickedCopyFile() {
+        // Given
+        val presenter = createInstanceToTest()
+        val file = FileTest.createFakeFile()
+        presenter.onFileChanged(file, "")
+
+        // When
+        presenter.onCopyClicked()
+
+        // Then
+        verify(fileCopyCutManager)!!.copy(file.path)
+    }
+
+    @Test
+    fun onCutClickedCutFile() {
+        // Given
+        val presenter = createInstanceToTest()
+        val file = FileTest.createFakeFile()
+        presenter.onFileChanged(file, "")
+
+        // When
+        presenter.onCutClicked()
+
+        // Then
+        verify(fileCopyCutManager)!!.cut(file.path)
+    }
+
+    private fun createInstanceToTest(): FileRowPresenter {
+        return FileRowPresenter(
+                screen!!,
+                fileDeleteManager!!,
+                fileCopyCutManager!!
+        )
+    }
+}
